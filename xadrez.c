27415483c9bc67b250e230b19f18f2b36cae769c
Thiago Barros <thiago.rck00@gmail.com>
2025-02-22 00:53:34 -0300

    Nível Aventureiro

--- xadrez.c
+++ xadrez.c
@@ -51,6 +51,15 @@ int main() {
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
 
+    printf("Movimentação do Cavalo: %d casas\n", movAmount);
+
+    for (int i = 0; i < movAmount; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("Cima\n");
+        }
+        printf("Esquerda\n");
+    }
+
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
     // Exemplo: Crie uma função recursiva para o movimento do Bispo.
